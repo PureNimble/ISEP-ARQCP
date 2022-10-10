35e80e5eabe735872a4714ac41aa8054c69aace7
joaopinto15 <1221694@isep.ipp.pt>
2022-10-10 22:40:37 +0100

    inicio_ex01

---
+++ modulo1/ex01/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+	//Declaring the variables
+	int x = 5; 
+	int *xPtr = &x;
+	float y = *xPtr + 3;
+	int vec[] = {10, 11, 12, 13};
+
+	//print of the value and adresses of the variables
+	printf("The value of x = %i \n",x);
+	printf("The value of y = %f \n",y);
+	printf("The adress of x = %p \n",&x);
+	printf("The adress of xPtr = %p \n",&xPtr);
+	printf("The value pointed by xPtr = %i \n",*xPtr);
+	printf("The adress of vec = %p \n",&vec);
+	
+	
+	for (int i=0;i < 4; i++)
+	{
+		printf("The value of vec[%i] = %i \n",i,vec[i]);
+	}
+	
+	for (int i=0;i < 4; i++)
+	{
+		printf("The adress of vec[%i] = %p \n",i,&vec[i]);
+	}
+	
+	return 0;
+}
