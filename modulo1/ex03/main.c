07c11afedbacf75c780c185532a1558638b85771
joaopinto15 <1221694@isep.ipp.pt>
2022-10-13 10:49:58 +0100

    ex03

--- modulo1/ex03/main.c
+++ modulo1/ex03/main.c
@@ -3,20 +3,28 @@
 int main()
 {
 	
+	//Declaration of variables
 	
-    int size;
-    
-    printf("Enter the size of the array: ");
-    scanf("%i",&size);
-    
-    int arr[size];
-    
-    for(int i=0; i<size; i++){
-			printf("Enter the Array elements: ");
-            scanf("%i",&arr[i]);
-    }
-    sum_even(arr, size);
-    
+		int size;
+
+    //Print and scan size of array
+		printf("Enter the size of the array: ");
+		scanf("%i",&size);
+		printf("\n");
+    //Declaration of the array
+		int arr[size];
+	
+	//Scan the elements of the array
+		for(int i=0; i<size; i++){
+				printf("Enter the Array elements: ");
+				scanf("%i",&arr[i]);
+		}
+    //Use of the function sum_even
+		int evenSum = sum_even(arr, size);
+		
+    //Final print of the return
+		printf("\nThe sum of even numbers are: %i",evenSum);
+		printf("\nThe address of the array is: %p \n", arr);
     
     return 0;
 }
--- modulo1/ex03/sum_even.c
+++ modulo1/ex03/sum_even.c
@@ -5,13 +5,9 @@ int sum_even( int *p, int num)
 	int evenSum =0;
     for(int i=0; i<num; i++)
     {
-        if(p[i]%2==0){
-         evenSum=evenSum+p[i];
+        if(*(p+i)%2==0){
+         evenSum=evenSum+*(p+i);
         }
     }
-    printf("\nThe sum of even numbers are: %i",evenSum);
-    printf("\nThe address of the array is: %p", p);
-    
-    return 0;
-    
+    return evenSum;    
 }
