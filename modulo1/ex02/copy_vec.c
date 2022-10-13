7a88478d9e6ac0042566e357e34e7b11785ada57
joaopinto15 <1221694@isep.ipp.pt>
2022-10-13 14:23:29 +0100

    ex02

--- modulo1/ex02/copy_vec.c
+++ modulo1/ex02/copy_vec.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-void copy_vec(int *vec1, int *vec2, int n){
+void copy_vec(int *vec1, int *vec2, int n)
+{	
+	//Copying the elements of vec1 to vec2 
+	for (int i = 0; i<n; i++){
+			
+		*vec2 = *vec1;
+		
+		vec1++;
+		vec2++;
 
-                for (int i = 0; i<n; i++){
-
-                *(vec2+i) = *(vec1+i);
-
-                }
+	}
 
 }
