eda3c22c9ad5e6770ee58c8708512f85682c17c1
joaopinto15 <1221694@isep.ipp.pt>
2022-10-13 14:37:09 +0100

    ex03

--- modulo1/ex03/sum_even.c
+++ modulo1/ex03/sum_even.c
@@ -2,12 +2,14 @@
 
 int sum_even( int *p, int num) 
 {
-	int evenSum =0;
+	int evenSum = 0;
     for(int i=0; i<num; i++)
     {
-        if(*(p+i)%2==0){
-         evenSum=evenSum+*(p+i);
+        if((*p)%2==0){
+         evenSum=evenSum+(*p);
+       
         }
+		p++;   
     }
     return evenSum;    
 }
