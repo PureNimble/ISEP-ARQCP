817351a1c25493a5e95cfd16482b48ceb2431833
joaopinto15 <1221694@isep.ipp.pt>
2022-10-13 13:47:21 +0100

    ex09

--- modulo1/ex09/sort_without_reps.c
+++ modulo1/ex09/sort_without_reps.c
@@ -1,34 +1,38 @@
 //function
 int sort_without_reps(int *src, int n, int *dest) 
 {
-    int i;
-    int j;
-    int k;
-    int a;
-    int counter =  n;
+	//Declaration of the variables
+		int i;
+		int j;
+		int k;
+		int a;
+		int counter =  n;
+	//Pass the elements of src to dest
     for(i=0;i<n;i++){
-        dest[i] = src[i];
+        *(dest+i) = *(src+i);
     }
+    //Remove the repeated numbers
     for(i=0;i<n;i++){
         for(j = i+1; j < n; j++){
-            if(dest[i] == dest[j]){
+            if((int)*(dest+i) == (int)*(dest+j)){
 				counter--;
                 for(k = j; k <n; k++){
-                    dest[k] = dest[k+1];
+                    *(dest+k) = *((dest+k)+1);
                 }
                 j--;
                 n--;
             }
         }
     }
+    //Put the elements in order
     for (i = 0; i < n; ++i) {
         for (j = i + 1; j < n; ++j)
         {
-            if (dest[i] > dest[j]) 
+            if (*(dest+i) > *(dest+j)) 
             {
-                a =  dest[i];
-                dest[i] = dest[j];
-                dest[j] = a;
+                a =  *(dest+i);
+                *(dest+i) = *(dest+j);
+                *(dest+j) = a;
             }
         }
      }
