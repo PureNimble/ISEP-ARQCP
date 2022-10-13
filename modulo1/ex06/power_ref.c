e4169027caef61be4097018cea84fa0cc891d1b6
joaopinto15 <1221694@isep.ipp.pt>
2022-10-13 18:01:24 +0100

    ex06

--- modulo1/ex06/power_ref.c
+++ modulo1/ex06/power_ref.c
@@ -1,10 +1,11 @@
 void power_ref(int *x, int y){
 	//Declaration of variables
 		int resultado = 1;
-    while (y != 0)
-    {
-        resultado *= *x;
-        --y;
-    }
-    *x = resultado;
+	//Calculation
+		while (y != 0)		// Number of times the number is multiplied
+		{
+			resultado *= *x; 	// Multipling the number and storing at -> 'resultado'
+			--y;				// Decreasing the counter	
+		}
+		*x = resultado;   //final value goes to the pointer
 }
