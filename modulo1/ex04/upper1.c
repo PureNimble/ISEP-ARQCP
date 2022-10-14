17b0acdd74c6b00a56d6055e38f8479c4fe5d4ef
joaopinto15 <1221694@isep.ipp.pt>
2022-10-14 18:45:27 +0100

    ex04

--- modulo1/ex04/upper1.c
+++ modulo1/ex04/upper1.c
@@ -1,9 +1,9 @@
-
 void upper1(char *str) {
     
-    for (int i = 0; str[i] != 0; i++){
-		if(str[i] >= 97 && str[i] <= 122){ //97 is "a" and 122 is "z"
-			str[i] = str[i] - 32;          //-32 is the upper char of the same letter
-		}
-	}
+    for(int i = 0; i < 30; i++) {
+        if(str[i] >= 65 + 32 && str[i] <= 90 + 32) { //65+32 is "a" and 90+32 is "z"
+            str[i] = str[i] - 32;				     //-32 is the upper char of the same letter
+        }
+        
+    }
 }
