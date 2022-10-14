bfad16977d50921e73ee80eb80ce844ddc7cc6b9
joaopinto15 <1221694@isep.ipp.pt>
2022-10-14 11:41:15 +0100

    ex14

--- modulo1/ex14/frequencies.c
+++ modulo1/ex14/frequencies.c
@@ -1,17 +1,21 @@
 
 void frequencies(float *grades, int n, int *freq) 
 {
-	 int vecI[n];
-	 for(int i = 0; i<n; i++){
-        vecI[i] = (int)*grades;
-        grades++;
-        }
-     int *ptr = freq;
-     for(int i = 0; i<n; i++){
-		 
-		ptr = freq+ vecI[i] ;
-        *ptr = *ptr + 1;
-        ptr = freq;
-        
-        }
+	//Declaration of a array
+		int vecI[n];
+	//Converte floats into ints and puting the elements in a array
+		for(int i = 0; i<n; i++){
+			vecI[i] = (int)*grades;
+			grades++;
+			}
+	//Declaration of a pointer
+		int *ptr = freq;
+	//Puting the values of vecI[] in the adress of freq
+		for(int i = 0; i<n; i++){
+			 
+			ptr = freq+ vecI[i] ;
+			*ptr = *ptr + 1;
+			ptr = freq;
+			
+			}
 }
--- modulo1/ex14/main.c
+++ modulo1/ex14/main.c
@@ -2,27 +2,31 @@
 #include "frequencies.h"
 int main()
 {
-    
-    int n;
-    printf("\nQual o número de alunos? ");
-    scanf("%i", &n);
-    float vec[n];
-    int vecNotas[21];
-    
-    for(int i = 0; i<n; i++){
-        printf("\nQual a nota do %iº aluno? ", i+1);
-        scanf("%f", &vec[i]);
-        }
-    for(int i = 0; i<n; i++){
-        printf("\nA nota do %iº aluno é %f\n", i+1, vec[i]);
-        }
-    frequencies(vec, n, vecNotas);     
-    
-    printf("\n{");
-    for(int i = 0; i<21; i++){
-        printf("%i ",vecNotas[i]);
-        }
-    printf("}\n");
+    //Declaration of the variables
+		int n;
+	//Print and scan of the size of the array
+		printf("\nQual o número de alunos? ");
+		scanf("%i", &n);
+	//Declaration of the arrays
+		float vec[n];
+		int vecNotas[21];
+    // Print and scan the elements of a array
+		for(int i = 0; i<n; i++){
+			printf("\nQual a nota do %iº aluno? ", i+1);
+			scanf("%f", &vec[i]);
+			}
+	//Print the elements if the array
+		for(int i = 0; i<n; i++){
+			printf("\nA nota do %iº aluno é %f\n", i+1, vec[i]);
+			}
+    //Use of the function
+		frequencies(vec, n, vecNotas);     
+    //Final print
+		printf("\n{");
+		for(int i = 0; i<21; i++){
+			printf("%i ",vecNotas[i]);
+			}
+		printf("}\n");
 }
 
 
