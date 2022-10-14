7516d99abec7ad46b6196f5b2b3dc1f1e9f66dd6
joaopinto15 <1221694@isep.ipp.pt>
2022-10-14 11:27:43 +0100

    ex03

--- modulo1/ex03/sum_even.c
+++ modulo1/ex03/sum_even.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
 
 int sum_even( int *p, int num) 
 {
-	int evenSum = 0;
-    for(int i=0; i<num; i++)
-    {
-        if((*p)%2==0){
-         evenSum=evenSum+(*p);
-       
-        }
-		p++;   
-    }
+	//Declaration of a variable
+		int evenSum = 0;
+	//Function that given a element of the array, divide the number by 2 to identify if the number is even
+		for(int i=0; i<num; i++)
+		{
+			if((*p)%2==0){
+			 evenSum=evenSum+(*p);
+		   
+			}
+			p++;   
+		}
     return evenSum;    
 }
