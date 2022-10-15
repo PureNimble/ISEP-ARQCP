d109d4e8c158056c4c1b7216727451786a12a720
joaopinto15 <1221694@isep.ipp.pt>
2022-10-15 15:48:03 +0100

    ex17

--- modulo1/ex17/swap.c
+++ modulo1/ex17/swap.c
@@ -4,6 +4,7 @@ void swap(int* vec1, int* vec2, int size)
 {
 	//Declaration of the variables 
 	int t = 0;
+	//Swapping the elements
     for (int i=0;i<size;i++) {
         t=*(vec2+i);
         *(vec2+i)=*(vec1+i);
