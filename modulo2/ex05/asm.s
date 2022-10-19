20f9d9e4805e999118b9c2bbcac024be0ad556d9
joaopinto15 <1221694@isep.ipp.pt>
2022-10-19 23:03:14 +0100

    ex05

--- modulo2/ex05/asm.s
+++ modulo2/ex05/asm.s
@@ -9,10 +9,10 @@ swapBytes:
 
 	movw s(%rip), %ax 	#place  ax =al + ah
 	
-	movb %al, %cl     	# move the least significant
-	movb %ah, %al	
-	addb %al, %al   						
-	movb %cl, %ah		
+	movb %al, %cl     	# move the least significant to a register
+	movb %ah, %al		# move the most significant to the least one
+	addb %al, %al   	# multiply the previous most significant byte by 2						
+	movb %cl, %ah		# move the register to the most significant byte
 	
 
 	ret            #return the value in %eax
