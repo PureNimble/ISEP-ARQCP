ae75fc10ec4b14125ca4edbcd8c244df26895156
joaopinto15 <1221694@isep.ipp.pt>
2022-10-21 20:00:57 +0100

    ex07

--- modulo2/ex07/asm.s
+++ modulo2/ex07/asm.s
@@ -8,8 +8,11 @@
 
 crossSumBytes:
 	
+	
+	
 	movw s1(%rip), %ax	#Put byte1 in the least significant byte
 	movw s2(%rip), %bx	#Put byte2 int the most significant byte
 	addb %bl, %ah    	#Sum the most significant bytes1 with the least significant byte of s2
 	addb %bh, %al		#Sum the most significant bytes2 with the least significant byte of s1
+	
 	ret            #return the value in %ax
--- modulo2/ex07/main.c
+++ modulo2/ex07/main.c
@@ -4,13 +4,13 @@
 int main(void)
 {
 	printf("Valor s1:");
-	scanf(" %hd ",&s1);
+	scanf(" %hd",&s1);
 	
 	printf("Valor s2:");
-	scanf(" %hd ",&s2);
+	scanf(" %hd",&s2);
 	
 	short ret = crossSumBytes();
 	
-	printf("Value of s after the byte swap: %hd\n",ret);
+	printf("Final value: %hd\n",ret);
 	return 0;
 }
