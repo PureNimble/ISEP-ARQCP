1b11fe6a3330f28cebde0eb1b928373d037ba9dd
joaopinto15 <1221694@isep.ipp.pt>
2022-11-06 16:18:58 +0000

    ex16

--- modulo3/ex14/asm.s
+++ modulo3/ex14/asm.s
@@ -17,24 +17,24 @@ exists:
 	movl num(%rip), %ecx		#move the size of vec to ecx
 	movl x(%rip),%eax			#move the search number to eax
 	movw $0, %di				#clean the counter
-	cmpl $0, %ecx			#check if vec is null
+	cmpl $0, %ecx				#check if vec is null
 	jz end
 
 loop:
 
-	cmpl %eax, (%rsi)   	
-	je   same    		
-	jmp  next				
+	cmpl %eax, (%rsi)   		# check if x = vec[i]
+	je   same    				# jmp if equal
+	jmp  next					# next number			
 
 same:
-	incw %di
+	incw %di					# increase the counter
 next:
-	addq $4,%rsi			#move to next number	
-	loop loop         		# loop
+	addq $4,%rsi				#move to next number	
+	loop loop         			# loop
 	
 		
-	cmpw $1,%di
-	jg duplicate
+	cmpw $1,%di					# check if the number is duplicate
+	jg duplicate				#
 	movl $0,%eax
 	jmp end
 
@@ -46,15 +46,8 @@ end:
 	ret 
 ##########################################################
 vec_diff:
-		
-	movq $0,%rdi
-	movl x(%rip),%edi
-	movl num(%rip),%eax
 	
-	cmpl $1,%edi
-	jle end_2
-	
-	subl %edi, %eax
-end_2:	
+	call exists	
+
 	ret
 ##########################################################
