18cc02de848e217f812e650a0d16acc8b984be97
joaopinto15 <1221694@isep.ipp.pt>
2022-11-09 16:26:26 +0000

    ex08

--- modulo4/ex08/asm.s
+++ modulo4/ex08/asm.s
@@ -13,15 +13,15 @@ calc:
 	
 	movl (%rsi),%eax			#move b to return
 	
-	sall $1, %edi				# a * 2			
-	subl %edi, %eax				# (a * 2) - b
+	sall $1, %edi				#  a * 2			
+	subl %eax, %edi				# (a * 2) - b
 	
 	subl $4, %edx				# c - 4
-	
-	imull %edx					# (c - 4) * ((a * 2) - b)
-	not %eax 					
-	incl %eax
+
+	imull %edx, %edi			# (c - 4) * ((a * 2) - b)
+
+	movl %edi,%eax				
 end:
-	ret						#return the value in eax
+	ret							#return the value in eax
 	
 #############################################	
