1daaf2b92739d5671f934eaaf32cd9c45ced87de
joaopinto15 <1221694@isep.ipp.pt>
2022-11-21 19:49:45 +0000

    ex18b

--- modulo4/ex18a/c.c
+++ modulo4/ex18a/c.c
@@ -1,13 +1,9 @@
 void changes(int *ptr){
-	char bits = 16;					// start of the third byte		
+	
 	int check = *ptr;				// initiation of the variable	
 	check &= 0xFF00;				// number & mask = just the second byte
 	check >>= 8;					// second byte
 	if(check > 15) {				
-		while ( bits != 24) {		// end at the last bit of the 3byte
-			*(ptr) ^= (1 << bits);	// num ^ (1 << bits) = inverte the bits
-
-			bits++;					// next bit
-		}
+		*(ptr) ^= 0xFF0000;			// inverte the bits in the third byte
 	}
 }
--- modulo4/ex18b/asm.s
+++ modulo4/ex18b/asm.s
@@ -16,24 +16,15 @@ changes:
 	movq %rsp, %rbp 	# copy the current stack pointer to RBP
 	
 #------------------------------------------------------
-	movl (%rdi), %eax
-	movb $16, %cl
-	movb $1,%dil
-	rorl $8, %eax
+	movl (%rdi), %eax	# move the number to eax
+	andl $0xff00,%eax	# number & mask = just the second byte
+	shrl $8, %eax		# second byte
 	
-	cmpb $15,%al
+	cmpl $15,%eax    	# check if the second byte is greater then 15
 	jle end
+	
+	xorl $0xFF0000,(%rdi)	# inverte the bits in the third byte
 
-loop:	
-	cmpb $24,%cl
-	je end
-	
-	shl %cl,%dil
-	
-	xorb %dil,(%rdi)
-	
-	incb %cl
-	jmp loop
 #------------------------------------------------------
 end:		
 	#epiloque 
