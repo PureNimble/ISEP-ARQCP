a9c83a18842f18d16d4ff04fc34809b4254ef650
joaopinto15 <1221694@isep.ipp.pt>
2022-11-21 18:55:01 +0000

    ex18a

---
+++ modulo4/ex18a/c.c
@@ -0,0 +1,16 @@
+void changes(int *ptr){
+	char bits = 16;					// start of the third byte		
+	int check = *ptr;				// initiation of the variable	
+	check &= 0xFF00;				// number & mask = just the second byte
+	check >>= 8;					// second byte
+	if(check > 15) {				
+		while ( bits != 24) {		// end at the last bit of the 3byte
+			*(ptr) ^= (1 << bits);	// num ^ (1 << bits) = inverte the bits
+
+			bits++;					// next bit
+		}
+	}else{
+		
+		
+	}
+}
