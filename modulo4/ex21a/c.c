7193afe8a93e5262bbb49ab2c0788622f38b5219
joaopinto15 <1221694@isep.ipp.pt>
2022-11-22 18:44:13 +0000

    ex21a

--- modulo4/ex21a/c.c
+++ modulo4/ex21a/c.c
@@ -1,13 +1,13 @@
 int distance(char *a, char *b){
-	int sum = 0;
-	while(*a != 0){
+	int sum = 0;				// declaration of the counter	
+	while(*a != 0){				// check the end of the string
 		
-		if(*a != *b){sum ++;}	
-		a++;
-		b++;	
+		if(*a != *b){sum ++;}	// count the number of wrong chars
+		a++;					// next char a
+		b++;					// next char b
 	}
-	if (*b != 0){
+	if (*b != 0){				// check if b have the same size
 		sum = -1;
 	}
-	return sum;
+	return sum;					// return the value
 }
